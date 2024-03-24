805a9bb1923fbc46e29504e8e9e7560858aabcb3
Vance-ng-vn <vance.ng.vn@gmail.com>
2024-03-24 19:30:40 +0700

    update

--- handler.js
+++ handler.js
@@ -5,13 +5,22 @@ const urlParams = new URLSearchParams(queryString);
 const targetSite = urlParams.get('target');
 const backSite = urlParams.get('back');
 
-function handler(){
-    goTarget();
-    goBack();
+function handler() {
+    if(backSite && targetSite) {
+        goTarget(targetSite);
+        goBack(backSite);
+    }
+    else if(!backSite && targetSite) {
+        goBack(targetSite, false);
+    }
+    else if(backSite && !targetSite) {
+        goBack(backSite);
+    }
 }
 
-function goBack(url) {
-    window.location = url + (url.includes('?') ? '&key=' + new Date().getTime() : '?key=' + new Date().getTime());
+function goBack(url, addParam = true) {
+    if(addParam) window.location = url + (url.includes('?') ? '&key=' + new Date().getTime() : '?key=' + new Date().getTime());
+    else window.location = url;
 }
 
 function goTarget(url) {
@@ -25,6 +34,6 @@ function goAnotherLanding() {
 
     const currentId = landingList.findIndex(landing => landing.includes(window.location.hostname));
     const nextId = currentId + 1;
-    if(nextId < landingList.length) goBack(landingList[nextId] + queryString);
-    else handler();
+    if(nextId >= landingList.length) alert('We cannot confirm that you are human.');
+    else goBack(landingList[nextId] + queryString, false);
 }
