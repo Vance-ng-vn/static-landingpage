6ff690cfb09bed87987fe9ab3dcbc82028dfd934
Vance-ng-vn <vance.ng.vn@gmail.com>
2024-03-28 15:13:12 +0700

    update

--- handler.js
+++ handler.js
@@ -3,25 +3,19 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
 const targetSite = urlParams.get('target');
-const fetchBack = urlParams.get('fetchback');
 
 function handler() {
-    if(fetchBack && targetSite) {
-        fetch(fetchBack).then(() => goTarget(targetSite));
-    }
-    else if(fetchBack) {
-        fetch(fetchBack);
-    }
-    else if(targetSite) {
-        goTarget(targetSite);
-    }
+    window.opener?.postMessage('handler', '*');
+
+    if(targetSite) goTarget(targetSite);
 }
 
 function goTarget(url) {
-    window.open(url, '_self');
+    window.location.href = url;
 }
 
 function goAnotherLanding() {
+    window.opener?.postMessage('goAnotherLanding', '*');
     const landingList = [
         'https://bot-verify.onrender.com/',
         'https://bot-detector.onrender.com/',
@@ -33,5 +27,5 @@ function goAnotherLanding() {
     const currentId = landingList.findIndex(landing => landing.includes(window.location.hostname));
     const nextId = currentId + 1;
     if(nextId >= landingList.length) alert('We cannot confirm that you are human.');
-    else goBack(landingList[nextId] + queryString);
+    else goTarget(landingList[nextId] + queryString);
 }
