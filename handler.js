14f3407f7c0d1328fc02ae6b4b38ddd1799aae57
Vance-ng-vn <vance.ng.vn@gmail.com>
2024-03-28 15:49:36 +0700

    Update

--- handler.js
+++ handler.js
@@ -7,25 +7,8 @@ const targetSite = urlParams.get('target');
 function handler() {
     window.opener?.postMessage('handler', '*');
 
-    if(targetSite) goTarget(targetSite);
+    if(targetSite) window.location.href = targetSite;
 }
-
-function goTarget(url) {
-    window.location.href = url;
-}
-
 function goAnotherLanding() {
     window.opener?.postMessage('goAnotherLanding', '*');
-    const landingList = [
-        'https://bot-verify.onrender.com/',
-        'https://bot-detector.onrender.com/',
-        'https://bot-notify.onrender.com/',
-        'https://bot-warning.onrender.com/',
-        'https://human-verify.onrender.com/'
-    ];
-
-    const currentId = landingList.findIndex(landing => landing.includes(window.location.hostname));
-    const nextId = currentId + 1;
-    if(nextId >= landingList.length) alert('We cannot confirm that you are human.');
-    else goTarget(landingList[nextId] + queryString);
 }
